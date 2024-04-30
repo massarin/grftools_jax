da725041235bc9a4e38606329c3cac0171688735
Nicolo Massari <nmassari@student.ethz.ch>
2024-05-01 00:13:36 +0200

    initial push

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grftools",
+    description="Collection of tools for Gaussian random fields",
+    author="Tilman Tröster",
+    author_email="tilman@troester.space",
+    version=0.2,
+    packages=find_packages(),
+)
--- grftools/threedee/random_fields.py
+++
@@ -1,3 +0,0 @@
-import grftools.power_spectrum_tools
-
-pseudo_Pofk = grftools.power_spectrum_tools.pseudo_Pofk
